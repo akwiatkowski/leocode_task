49ecda2ebd771d4d9080ab46622130e3a295b1ca
Aleksander Kwiatkowski <bobikx@poczta.fm>
2020-11-13 09:36:35 +0100

    restruct spec

--- spec/checkout_spec.rb
+++ spec/checkout_spec.rb
@@ -1,64 +1,75 @@
 # frozen_string_literal: true
 
 RSpec.describe Checkout do
-  # TODO: better structurize tests
-  it "calculates total price - 1 product" do
-    co = Checkout.new(promotional_rules)
-    co.scan("001")
-    price = co.total
+  context "when only product 001 is scanned" do
+    it "calculates simple total price" do
+      co = Checkout.new(promotional_rules)
+      co.scan("001")
+      price = co.total
 
-    expect(price).to eq("£9.25")
+      expect(price).to eq("£9.25")
+    end
   end
 
-  it "calculates total price - over £60" do
-    co = Checkout.new(promotional_rules)
-    # 2 times "002" is 2x45, it's over £60
-    co.scan("002")
-    co.scan("002")
-    price = co.total
+  context "when 2 * 002 products are scanned" do
+    it "calculates proper total price using total promotion" do
+      co = Checkout.new(promotional_rules)
+      # 2 times "002" is 2x45, it's over £60
+      co.scan("002")
+      co.scan("002")
+      price = co.total
 
-    # £90.00 minus 10% => £81
-    expect(price).to eq("£81.00")
+      # £90.00 minus 10% => £81
+      expect(price).to eq("£81.00")
+    end
   end
 
-  it "calculates total price - 2 Red Scarf" do
-    co = Checkout.new(promotional_rules)
-    # 2 times "001" changes price from 9.25 to 8.50
-    co.scan("001")
-    co.scan("001")
-    price = co.total
+  context "when 2 * 001 products are scanned" do
+    it "calculates proper total price using quantity promotion" do
+      co = Checkout.new(promotional_rules)
+      # 2 times "001" changes price from 9.25 to 8.50
+      co.scan("001")
+      co.scan("001")
+      price = co.total
 
-    expect(price).to eq("£17.00")
+      expect(price).to eq("£17.00")
+    end
   end
 
-  it "calculates total price - sample 1" do
-    co = Checkout.new(promotional_rules)
-    co.scan("001")
-    co.scan("002")
-    co.scan("003")
-    price = co.total
+  context "when 001, 002, 003 are scanned" do
+    it "calculates proper total price" do
+      co = Checkout.new(promotional_rules)
+      co.scan("001")
+      co.scan("002")
+      co.scan("003")
+      price = co.total
 
-    expect(price).to eq("£66.78")
+      expect(price).to eq("£66.78")
+    end
   end
 
-  it "calculates total price - sample 2" do
-    co = Checkout.new(promotional_rules)
-    co.scan("001")
-    co.scan("003")
-    co.scan("001")
-    price = co.total
+  context "when 001, 003, 001 are scanned" do
+    it "calculates proper total price" do
+      co = Checkout.new(promotional_rules)
+      co.scan("001")
+      co.scan("003")
+      co.scan("001")
+      price = co.total
 
-    expect(price).to eq("£36.95")
+      expect(price).to eq("£36.95")
+    end
   end
 
-  it "calculates total price - sample 3" do
-    co = Checkout.new(promotional_rules)
-    co.scan("001")
-    co.scan("002")
-    co.scan("001")
-    co.scan("003")
-    price = co.total
+  context "when 001, 002, 001, 003 are scanned" do
+    it "calculates proper total price" do
+      co = Checkout.new(promotional_rules)
+      co.scan("001")
+      co.scan("002")
+      co.scan("001")
+      co.scan("003")
+      price = co.total
 
-    expect(price).to eq("£73.76")
+      expect(price).to eq("£73.76")
+    end
   end
 end
